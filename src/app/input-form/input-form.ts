b36cc83e63c3b806b9dc9d70f4feaa0373ffcf49
hilda.chuang <hilda.chuang@gmail.com>
2025-12-17 09:47:23 +0800

    feat(w3d4-B2, 3): add html and ts contents

--- src/app/input-form/input-form.ts
+++ src/app/input-form/input-form.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-input-form',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule
+  ],
   templateUrl: './input-form.html',
   styleUrl: './input-form.scss',
   standalone: true
 })
 export class InputForm {
+  name = signal('');
+  submitValue = signal('');
 
+  onSubmit() {
+    this.submitValue.set(this.name())
+  }
 }
